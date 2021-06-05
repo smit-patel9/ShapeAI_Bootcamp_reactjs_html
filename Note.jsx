4d6ddb929ea338a57b27bab84e6226f023d0d996
smit9909 <84852855+smit9909@users.noreply.github.com>
2021-06-05 14:29:37 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Skills</h1>
+      <p>
+        I learned c++, Java, Python, My Sql,Bootstrap. 
+      </p>
+    </div>
+  );
+}
+
+export default Note;
